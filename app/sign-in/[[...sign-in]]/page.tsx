f59b05a00448cdc8de63215e7f88cc90773d9315
nk0311 <navaitk031@gmail.com>
2024-09-09 00:21:26 -0400

    finished clerk implementation

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
-
+import BgGradient from '@/components/common/bg-gradient';
 export default function Page() {
   return (
     <section className="flex justify-center items-center py-16">
-  <SignIn />
+  <BgGradient><SignIn /></BgGradient>
   </section>
   );
 }
